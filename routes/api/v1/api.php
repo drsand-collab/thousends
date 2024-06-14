378477189c86150c98a6345bd9e4d73fe70adeda
dralexsand <dr.alex.sand.111@gmail.com>
2024-06-14 11:43:32 +0500

    link.add routes

--- routes/api/v1/api.php
+++ routes/api/v1/api.php
@@ -1,19 +1,11 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\LinkController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::name('api.')->group(function () {
+    Route::prefix('link')->name('link.')->group(function () {
+        Route::get('/get', [LinkController::class, 'getLink'])->name('get');
+        Route::get('/generate', [LinkController::class, 'linkGenerate'])->name('generate');
+    });
 });
